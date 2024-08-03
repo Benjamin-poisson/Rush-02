4650d8f3db31fca4a333e6e8085c75747b82fe5d
Mae0165 <maelysyacoub@gmail.com>
2024-08-03 18:53:15 +0200

    gest error update

--- ex00/gest_error.c
+++ ex00/gest_error.c
@@ -6,7 +6,7 @@
 /*   By: myacoub <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 14:40:59 by myacoub           #+#    #+#             */
-/*   Updated: 2024/08/03 17:51:21 by myacoub          ###   ########.fr       */
+/*   Updated: 2024/08/03 18:52:42 by myacoub          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int	gest_error(int ac, char **av)
 {
 	int	i;
 	int	j;
-
 
 	i = 0;
 	if (ac > 3 || ac == 1)
@@ -36,20 +35,36 @@ int	gest_error(int ac, char **av)
 	}
 	return (0);
 }
-
-int	get_max_number(char **list)
+int	gest_error_dict(int ac, char **av)
 {
 	int	i;
 	int	j;
-	int	size;
-	while (list[j])
+
+	i = 0;
+	while (av[j][i])
 	{
-		i = 0;
-		while (list[j][i])
-			i++;
-		if (size < i)
-			size = i;
-		j++;
+		if (av[j][i] < '0' || av[j][i] > '9')
+		{
+			write(1, "Error\n", 6);
+			retrun (1);
+		}
+		i++;
 	}
-	return (size);
+	return (0);
+}
+int	get_max_number(int ac, char **av)
+{
+	int	i;
+
+	i = 0;
+	if (ac == 3)
+	{
+		while (av[1])
+			i++;
+		if (av[1][i] == 't' && av[1][i -1] == 'c' && av[1][i -2] == 'i' 
+		&& av[1][i -3] == 'd' && av[1][i -4] == '.')
+			return (0);
+		write(1, "Error\n", 6);
+		retrun (1);
+	}
 }
