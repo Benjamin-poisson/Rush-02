a3478db8a8ce263fc647e20e65540e1c5b453a31
Jordan Guaglio <jguaglio@c2r2p6.42perpignan.fr>
2024-08-04 10:59:50 +0200

    fim de l'affichage

--- ex00/include/puissance_10.c
+++ ex00/include/puissance_10.c
@@ -6,28 +6,12 @@
 /*   By: jguaglio <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 15:34:50 by jguaglio          #+#    #+#             */
-/*   Updated: 2024/08/03 19:05:47 by jguaglio         ###   ########.fr       */
+/*   Updated: 2024/08/04 10:57:51 by jguaglio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
-
-int	ft_strcmp(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i] != '\0' || s2[i] != '\0')
-	{
-		if (s1[i] > s2[i])
-			return ((int)(s1[i] - s2[i]));
-		else if (s1[i] < s2[i])
-			return (-(int)(s2[i] - s1[i]));
-		i++;
-	}
-	return (0);
-}
 
 int	ft_strlen(char *str)
 {
@@ -50,6 +34,22 @@ void	ft_putstr(char *str)
 		i++;
 	}
 	write(1, " ", 1);
+}
+
+int     is_power_ten(char * number)
+{
+        int     i;
+
+        i = 0;
+        while (number[i])
+        {
+                if (number[0] != '1')
+                        return (0);
+                if (i != 0 && number[i] != '0')
+                        return (0);
+                i++;
+        }
+        return (1);
 }
 
 void	ft_print_unity(char **list_number, char **list_name, char *unity)
@@ -82,11 +82,12 @@ void	ft_print_ten(char **list_number, char **list_name, char *ten, char *unity)
 		if (ft_strlen(list_number[i]) == 2 && list_number[i][0] == ten[0] && list_number[i][1] == '0')
 		{
 			ft_putstr(list_name[i]);
-			break ;
+			ft_print_unity(list_number, list_name, unity);
 		}
+		if (ft_strlen(list_number[i]) == 2 && list_number[i][0] == ten[0] && list_number[i][1] == unity[0])
+			ft_putstr(list_name[i]);
 		i++;
 	}
-	ft_print_unity(list_number, list_name, unity);
 }
 
 void	ft_print_hundred(char **list_number, char **list_name, char *hundred, char *ten, char *unity)
@@ -103,7 +104,7 @@ void	ft_print_hundred(char **list_number, char **list_name, char *hundred, char 
 		ft_print_unity(list_number, list_name, hundred);
 	while (list_number[i])
 	{
-		if (ft_strlen(list_number[i]) == 3) // et si cest bien 100 et pas 101
+		if (ft_strlen(list_number[i]) == 3 && is_power_ten(list_number[i]))
 		{
 			ft_putstr(list_name[i]);
 			break ;
@@ -113,41 +114,76 @@ void	ft_print_hundred(char **list_number, char **list_name, char *hundred, char 
 	ft_print_ten(list_number, list_name, ten, unity);
 }
 
+void	print_under_3digits(char **list_number, char **list_name, char *number)
+{
+	if (ft_strlen(number) == 3)
+		ft_print_hundred(list_number, list_name, &number[0], &number[1], &number[2]);
+	else if (ft_strlen(number) == 2)
+		ft_print_ten(list_number, list_name, &number[0], &number[1]);
+	else if (ft_strlen(number) == 1)
+		ft_print_unity(list_number, list_name, &number[0]);
+	return ;
+}
+
+typedef	struct s_incrementeur
+{
+	int	i;
+	int	j;
+} t_incrementeur;
+
+
+
+void    ft_print_power_of_ten2(char **list_number, char **list_name, char *number,
+                t_incrementeur *n, int *reduction)
+{
+        if (ft_strlen(number) == (ft_strlen(list_number[n->j]) + 1))
+        {
+                ft_print_ten(list_number, list_name, &number[0], &number[1]);
+                *reduction = 2;
+        }
+        else if (ft_strlen(number) == (ft_strlen(list_number[n->j]) + 2))
+        {
+                ft_print_hundred(list_number, list_name, &number[0], &number[1], &number[2]);
+                *reduction = 3;
+        }
+        ft_putstr(list_name[n->i - 1]);
+}
+
 
 
 char	*ft_print_power_of_ten(char **list_number, char **list_name, char *number)
 {
-	int	i;
+	t_incrementeur n;
 	int	reduction;
 
-	i = 0;
+	n.i = 0;
+	n.j = -1;
 	if (ft_strlen(number) <= 3)
-		return (number);
-	while (list_number[i])
 	{
-		if (ft_strlen(list_number[i]) == ft_strlen(number))
+		print_under_3digits(list_number, list_name, number);
+		return (number);
+	}
+	while (list_number[n.i++])
+	{
+		if (is_power_ten(list_number[n.i]))
 		{
-			ft_print_unity(list_number, list_name, &number[0]);
-			ft_putstr(list_name[i]);
-			reduction = 1; 
-			break;
-		}
-		else if (i !=0 && ft_strlen(number) < ft_strlen(list_number[i]) && ft_strlen(number) > ft_strlen(list_number[i - 1]))
-		{	
-			if (ft_strlen(number) == (ft_strlen(list_number[i - 1]) + 1))
+			if (n.j == -1)
+				n.j = n.i;
+			if (ft_strlen(list_number[n.i]) == ft_strlen(number))
 			{
-				ft_print_hundred(list_number, list_name,"0", &number[0], &number[1]);
-				reduction = 2;
+				ft_print_unity(list_number, list_name, &number[0]);
+				ft_putstr(list_name[n.i]);
+				reduction = 1; 
+				break;
 			}
-			else if (ft_strlen(number) == (ft_strlen(list_number[i - 1]) + 2))
+			else if (n.i != 0 && ft_strlen(number) < ft_strlen(list_number[n.i]) &&
+				       	ft_strlen(number) > ft_strlen(list_number[n.j]))
 			{
-				ft_print_hundred(list_number, list_name, &number[0], &number[1], &number[2]);
-				reduction = 3;
+				ft_print_power_of_ten2(list_number, list_name, number, &n, &reduction);
+				break ;
 			}
-			ft_putstr(list_name[i - 1]);
-			break ;
+			n.j = n.i;	
 		}
-		i++;
 	}
 	ft_print_power_of_ten(list_number, list_name, &number[reduction]);
 	return (number);
@@ -155,8 +191,8 @@ char	*ft_print_power_of_ten(char **list_number, char **list_name, char *number)
 
 int	main(void)
 {
-	char	*l1[] = {"2","3","30", "100", "1000", "1000000", 0};
-	char	*l2[] = {"deux","trois","trente", "cent", "mille", "million"};
+	char	*l1[] = {"2","3","30","101", "100", "1000", "1000000", 0};
+	char	*l2[] = {"deux","trois","trente","cent-un", "cent", "mille", "million"};
 	char	nbr[] = "2330132";
 
 	ft_print_power_of_ten(l1, l2, nbr);
